b9d78e3d5318a168a875fcfd6134766cf5383ac4
KID <50666207+KID421@users.noreply.github.com>
2021-10-25 16:36:05 +0800

    Update Vuforia 圖卡、動畫與虛擬按鈕

---
+++ AR_Vuforia_20211025/Assets/Scripts/ShowCanvas.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+namespace KID
+{
+    /// <summary>
+    /// 顯示畫布
+    /// </summary>
+    public class ShowCanvas : MonoBehaviour
+    {
+        [Header("畫布群組元件")]
+        public CanvasGroup group;
+
+        /// <summary>
+        /// 開始淡入
+        /// </summary>
+        public void StartFade(float increase)
+        {
+            StartCoroutine(FadeCanvas(increase));
+        }
+
+        /// <summary>
+        /// 畫布淡入：每隔 0.1 秒 添加 0.1 透明度執行十次
+        /// </summary>
+        /// <param name="increase">要增加得值，0.1 淡入，-0.1 淡出</param>
+        private IEnumerator FadeCanvas(float increase)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                group.alpha += increase;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
---
+++ AR_Vuforia_20211025/Assets/Scripts/TargetVirtualButtonManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Vuforia;
+
+namespace KID
+{
+    /// <summary>
+    /// 圖卡目標的虛擬按鈕管理器
+    /// </summary>
+    public class TargetVirtualButtonManager : MonoBehaviour
+    {
+        [Header("要監聽的虛擬按鈕")]
+        public VirtualButtonBehaviour vbbBite;
+        [Header("要控制的動畫元件")]
+        public Animator aniTarget;
+
+        private void Start()
+        {
+            // 虛擬按鈕 註冊按下後要執行的方法
+            vbbBite.RegisterOnButtonPressed(PlayBiteAnimation);
+        }
+
+        /// <summary>
+        /// 播放動畫方法，虛擬按鈕按下要執行的方法
+        /// </summary>
+        /// <param name="vbb">虛擬按鈕註冊用的參數</param>
+        private void PlayBiteAnimation(VirtualButtonBehaviour vbb)
+        {
+            print(vbb.name);
+        }
+    }
+}
